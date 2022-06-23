8a3fc91a177668ebebbc880174e153d16691c9a3
ejuju <juju.sel.mail@gmail.com>
2022-06-23 02:20:24 +0200

    update user agent test

--- pkg/netutil/user_agent_test.go
+++ pkg/netutil/user_agent_test.go
@@ -19,4 +19,14 @@ func TestRandUserAgent(t *testing.T) {
 			t.Fatal("user agent should not return the same string twice for rand.Seed(0)")
 		}
 	})
+
+	t.Run("should be able to use custom user-agent list", func(t *testing.T) {
+		list := []string{"a", "b"} // relies on rand.seed 0
+		if RandUserAgent(list) != list[0] {
+			t.Fatalf("user agent should be: %#v", list[0])
+		}
+		if RandUserAgent(list) != list[1] {
+			t.Fatalf("user agent should be: %#v", list[1])
+		}
+	})
 }
